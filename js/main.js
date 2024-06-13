89ee05ec83de1aba4c07898a6c6bd38ba1ac8b5c
111Makss111 <151084313+111Makss111@users.noreply.github.com>
2024-06-13 18:21:29 +0200

    .

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  loop: true,
+const swiper = new Swiper(".top-swiper", {
+  // loop: true,
+  effect: "fade",
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-
+});
+const swiperAbout = new Swiper(".about-slider", {
+  slidesPerView: 3.5,
+  spaceBetween: 20,
+  freeMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
